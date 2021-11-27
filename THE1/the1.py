8b71a444b3ed3289c75f3274bdbe8b82b8d1aa2d
Erce Güder <erce_guder@icloud.com>
2021-11-27 19:34:18 +0300

    add parse()

--- THE1/the1.py
+++ THE1/the1.py
@@ -18,12 +18,6 @@ def gaussians(m: list, s: list):
 
     return np.divide(mixture, np.sum(mixture))
 
-def normalize(intensities: np.ndarray):
-    g_m = np.subtract(intensities, min(intensities))
-    g_s = np.round(np.multiply(np.divide(g_m, max(g_m)), 255))
-
-    return g_s.astype(int)
-
 def find_closest(table: dict, s_k):
     diff = 255
     z_k = 0
@@ -33,13 +27,23 @@ def find_closest(table: dict, s_k):
             z_k = i
     return z_k
 
+def parse(path: str):
+    out_dir = None
+
+    if path.endswith('/'):
+        out_dir = "/".join(path.split('/')[:-1])
+    else:
+        out_dir = "/".join(path.split('/'))
+
+    return out_dir + '/'
+
 def part1(input_img_path: str, output_path: str, m: list, s: list):
     img = cv2.imread(input_img_path, cv2.IMREAD_GRAYSCALE)
     
     assert img is not None
 
     N = img.shape[0]*img.shape[1]                               # number of pixels
-    out_dir = "/".join(output_path.split('/')[:-1])
+    out_dir = parse(output_path)
 
     try:
         os.makedirs(out_dir)
@@ -59,20 +63,15 @@ def part1(input_img_path: str, output_path: str, m: list, s: list):
         h_c_org[i] += h_c_org[i-1]
 
     plt.hist(intensities, bins=50)
-    plt.savefig(out_dir + "/original_histogram.png")
+    plt.savefig(out_dir + "original_histogram.png")
     plt.clf()
 
     mixture = np.random.choice(256, N, True, gaussians(m, s))
     plt.hist(mixture, bins=50)
-    plt.savefig(out_dir + "/gaussian_histogram.png")
+    plt.savefig(out_dir + "gaussian_histogram.png")
     plt.clf()
     
     # Matching
-    # unique, counts = np.unique(gaussian, return_counts=True)
-    # print(dict(zip(unique, counts)))
-
-    # gaussian = normalize(gaussian)
-
     for intensity in mixture:                                   # h of mixture
         h_c_gaus[intensity] += 1
 
@@ -95,10 +94,10 @@ def part1(input_img_path: str, output_path: str, m: list, s: list):
             row[idx] = z_k[intensity]
             intensities.append(z_k[intensity])
 
-    cv2.imwrite(out_dir + "/matched_image.png", img)
+    cv2.imwrite(out_dir + "matched_image.png", img)
 
     plt.hist(intensities, bins=50)
-    plt.savefig(out_dir + "/matched_image_histogram.png")
+    plt.savefig(out_dir + "matched_image_histogram.png")
     
 def the1_convolution(input_img_path:str, filter: list):
     """
@@ -136,12 +135,9 @@ def the1_convolution(input_img_path:str, filter: list):
 
     return output
 
-def part2(input_img_path , output_path):
-    pass
-
 if __name__ == "__main__":
     ex = 1#input("Image: ")
-    part1(f"THE1-Images/{ex}.png", f"Outputs/{ex}/ex{ex}.png", [45, 200], [45, 10])
+    part1(f"./THE1-Images/{ex}.png", "./Outputs/Part1/", [30, 230], [5, 5])
 
     # box_filter = [  [1, 1, 1], 
     #                 [0, 0, 0],
